9c6f0638d809caa655d975cd18a02deffa3544cd
atsushi.koga <stbe51@gmail.com>
2020-01-03 12:48:08 +0900

    get request file data

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import(
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -35,6 +36,38 @@ func postForm(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	fmt.Fprintln(w, r.PostForm["first_name"])
 }
 
+func formValue(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// リクエストされたデータを対象に、構造体Formの中に該当する値が複数あっても、最初の要素しか取得しない
+	fmt.Fprintln(w, r.FormValue("first_name"))
+}
+
+func postFormValue(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// フォームのデータのみを対象に、構造体Formの中に該当する値が複数あっても、最初の要素しか取得しない
+	fmt.Fprintln(w, r.PostFormValue("first_name"))
+}
+
+func multipartForm(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	r.ParseMultipartForm(1024)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	file, err := fileHeader.Open()
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
+}
+
+func formFile(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	file, _, err := r.FormFile("uploaded")
+	if err == nil {
+		data, err := ioutil.ReadAll(file)
+		if err == nil {
+			fmt.Fprintln(w, string(data))
+		}
+	}
+}
+
 func main() {
 	mux := httprouter.New()
 	mux.GET("/hello/:name", hello)
@@ -42,6 +75,10 @@ func main() {
 	mux.POST("/body", body)
 	mux.POST("/form", form)
 	mux.POST("/post_form", postForm)
+	mux.POST("/form_value", formValue)
+	mux.POST("/post_form_value", postFormValue)
+	mux.POST("/multipart_form", multipartForm)
+	mux.POST("/form_file", formFile)
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 		Handler: mux,
