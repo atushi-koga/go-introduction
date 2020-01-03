cd6d056eb4ca94b37224b7c29daa1572968c345c
atsushi.koga <stbe51@gmail.com>
2020-01-03 11:33:50 +0900

    get request header and body

--- server.go
+++ server.go
@@ -7,17 +7,27 @@ import(
 )
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "Hello, %s\n", p.ByName("name"))
+	fmt.Fprintf(w, "World, %s\n", p.ByName("name"))
 }
 
-func world(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "World, %s\n", p.ByName("name"))
+func header(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(w, r.Header)
+	fmt.Fprintln(w, r.Header["Accept-Encoding"])
+	fmt.Fprintln(w, r.Header.Get("Accept-Encoding"))
+}
+
+func body(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	length := r.ContentLength
+	body := make([]byte, length)
+	r.Body.Read(body)
+	fmt.Fprintln(w, string(body))
 }
 
 func main() {
 	mux := httprouter.New()
 	mux.GET("/hello/:name", hello)
-	mux.GET("/world/:name", world)
+	mux.GET("/header/", header)
+	mux.POST("/body/", body)
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 		Handler: mux,
